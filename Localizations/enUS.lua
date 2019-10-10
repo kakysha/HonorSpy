0335ba0a2c310e88a6d04be07cd8e84ed2b991e8
kakysha <kakysha@me.com>
2019-10-11 02:48:58 +0300

    Classic started: Ace2 -> Ace3, core done, no GUI yet

---
+++ Localizations/enUS.lua
@@ -0,0 +1,56 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("HonorSpy", "enUS", true)
+
+if L then
+
+L["HonorSpy standings"] = true
+L["Name"] = true
+L["ThisWeekHonor"] = true
+L["LastWeekHonor"] = true
+L["Standing"] = true
+L["RP"] = true
+L["Rank"] = true
+L["d"] = true
+L["h"] = true
+L["m"] = true
+L["s"] = true
+
+L["Weekly data was reset"] = true
+L["This will purge ALL addon data, you sure?"] = true
+L["All data was purged"] = true
+L["Show HonorSpy Standings"] = true
+L["Report specific player standings"] = true
+L["player_name"] = true
+L["Player %s not found in table"] = true
+
+L["Report for player"] = true
+L["Pool Size"] = true
+L["Standing"] = true
+L["Bracket"] = true
+L["current RP"] = true
+L["Next Week RP"] = true
+L["Current Rank"] = true
+L["Next Week Rank"] = true
+L["HonorSpy options"] = true
+L["Sunday"] = true
+L["Monday"] = true
+L["Tuesday"] = true
+L["Wednesday"] = true
+L["Thursday"] = true
+L["Friday"] = true
+L["Saturday"] = true
+L["PvP Week Reset On"] = true
+L["Day of week when new PvP week starts (10AM UTC)"] = true
+L["Sort By"] = true
+L["Set up sorting column"] = true
+L["Export to CSV"] = true
+L["Show window with current data in CSV format"] = true
+L["Report My Standing"] = true
+L["Reports your current standing as emote"] = true
+L["_ purge all data"] = true
+L["Delete all collected data"] = true
+L["Limit Rows"] = true
+L["Limits number of rows shown in table"] = true
+L["<EP>"] = true
+L["Limit"] = true
+
+end
--- honorspy.lua
+++ honorspy.lua
@@ -1,40 +1,39 @@
-HonorSpy = AceLibrary("AceAddon-2.0"):new("AceConsole-2.0", "AceDB-2.0", "AceEvent-2.0", "AceModuleCore-2.0", "FuBarPlugin-2.0", "AceComm-2.0", "AceHook-2.1")
-local T = AceLibrary("Tablet-2.0")
-local L = AceLibrary("AceLocale-2.2"):new("HonorSpy")
+HonorSpy = LibStub("AceAddon-3.0"):NewAddon("HonorSpy", "AceConsole-3.0", "AceHook-3.0", "AceEvent-3.0", "AceComm-3.0", "AceSerializer-3.0")
 
-HonorSpy:RegisterDB("HonorSpyDB")
-HonorSpy:RegisterDefaults('realm', {
-	hs = {
-		currentStandings = {},
-		last_reset = 0,
-		sort = L["ThisWeekHonor"],
-		limit = 750
-	}
-})
+local L = LibStub("AceLocale-3.0"):GetLocale("HonorSpy", true)
 
-local commPrefix = "HonorSpy";
-HonorSpy:SetCommPrefix(commPrefix)
+local addonName = GetAddOnMetadata("HonorSpy", "Title");
+local commPrefix = addonName;
 
-local VERSION = 3;
+local VERSION = 1;
 local paused = false; -- pause all inspections when user opens inspect frame
 local playerName = UnitName("player");
 
-local RealmPlayersAddon = false;
-if (type(VF_InspectDone) ~= "nil" and type(VF_StartInspectingTarget) ~= "nil") then
-	RealmPlayersAddon = true;
+function HonorSpy:OnInitialize()
+    self.db = LibStub("AceDB-3.0"):New("HonorSpyDB", {
+    	factionrealm = {
+			currentStandings = {},
+			last_reset = 0,
+			sort = L["ThisWeekHonor"],
+			limit = 750,
+			minimapButton = {hide = false}
+    	}
+	}, true)
+
+	DrawMinimapIcon();
 end
 
 function HonorSpy:OnEnable()
-	self:Hook("InspectUnit");
-	self:RegisterComm(commPrefix, "GROUP", "OnCommReceive")
-	self:RegisterComm(commPrefix, "GUILD", "OnCommReceive")
-	-- self:RegisterComm(commPrefix, "CUSTOM", "HS", "OnCommReceiveCustom")
-	self:RegisterEvent("PLAYER_DEAD");
+	self:SecureHook("InspectUnit");
+
 	self:RegisterEvent("PLAYER_TARGET_CHANGED");
 	self:RegisterEvent("UPDATE_MOUSEOVER_UNIT");
+
 	self:RegisterEvent("INSPECT_HONOR_UPDATE");
-	self.OnMenuRequest = BuildMenu();
-	checkNeedReset();
+
+	self:RegisterComm(commPrefix, "OnCommReceive")
+
+	self:RegisterEvent("PLAYER_DEAD");
 end
 
 local inspectedPlayers = {}; -- stores last_checked time of all players met
@@ -55,8 +54,8 @@ local function StartInspecting(unitID)
 		or not CanInspect(unitID)) then
 		return
 	end
-	
-	local player = HonorSpy.db.realm.hs.currentStandings[name] or inspectedPlayers[name];
+
+	local player = HonorSpy.db.factionrealm.currentStandings[name] or inspectedPlayers[name];
 	if (player == nil) then
 		inspectedPlayers[name] = {last_checked = 0};
 		player = inspectedPlayers[name];
@@ -79,7 +78,7 @@ function HonorSpy:INSPECT_HONOR_UPDATE()
 		return;
 	end
 
-	local player = self.db.realm.hs.currentStandings[inspectedPlayerName] or inspectedPlayers[inspectedPlayerName];
+	local player = self.db.factionrealm.currentStandings[inspectedPlayerName] or inspectedPlayers[inspectedPlayerName];
 	if (player.class == nil) then player.class = "nil" end
 
 	local _, _, _, _, thisweekHK, thisWeekHonor, _, lastWeekHonor, standing = GetInspectHonorData();
@@ -91,101 +90,30 @@ function HonorSpy:INSPECT_HONOR_UPDATE()
 	ClearInspectPlayer();
 	NotifyInspect("target"); -- change real target back to player's target, broken by prev NotifyInspect call
 	ClearInspectPlayer();
-	if (RealmPlayersAddon) then
-		VF_TemporarySupressTargetChange = nil;
-		VF_PlayerChosenTarget = true;
-		VF_StartInspectingTarget();
-	end
+	
 	player.last_checked = time();
 	player.RP = 0;
 
-	if (thisweekHK >= 15) then
+	if (thisweekHK >= 0) then
 		if (player.rank >= 3) then
 			player.RP = math.ceil((player.rank-2) * 5000 + player.rankProgress * 5000)
 		elseif (player.rank == 2) then
 			player.RP = math.ceil(player.rankProgress * 3000 + 2000)
 		end
-		self.db.realm.hs.currentStandings[inspectedPlayerName] = player;
-		self:SendCommMessage("GROUP", inspectedPlayerName, player);
-		self:SendCommMessage("GUILD", inspectedPlayerName, player);
-		-- self:SendCommMessage("CUSTOM", "HS", inspectedPlayerName, player);
+		self.db.factionrealm.currentStandings[inspectedPlayerName] = player;
+		self:SendCommMessage(commPrefix, self:Serialize(inspectedPlayerName, player), "GROUP");
+		self:SendCommMessage(commPrefix, self:Serialize(inspectedPlayerName, player), "GUILD");
 	end
 	inspectedPlayers[inspectedPlayerName] = {last_checked = player.last_checked};
 	inspectedPlayerName = nil;
 end
 
--- RESET WEEK
-function resetWeek(must_reset_on)
-	HonorSpy.db.realm.hs.last_reset = must_reset_on;
-	inspectedPlayers = {};
-	HonorSpy.db.realm.hs.currentStandings={};
-	HonorSpyStandings:Refresh();
-	HonorSpy:Print(L["Weekly data was reset"]);
-end
-function checkNeedReset()
-	if (HonorSpy.db.realm.hs.reset_day == nil) then HonorSpy.db.realm.hs.reset_day = 3 end
-	local day = date("!%w");
-	local h = date("!%H");
-	local m = date("!%M");
-	local s = date("!%S");
-	local days_diff = (7 + (day - HonorSpy.db.realm.hs.reset_day)) - math.floor((7 + (day - HonorSpy.db.realm.hs.reset_day))/7) * 7;
-	local diff_in_seconds = s + m*60 + h*60*60 + days_diff*24*60*60 - 10*60*60 - 1; -- 10 AM UTC - fixed hour of PvP maintenance
-	if (diff_in_seconds > 0) then -- it is negative on reset_day untill 10AM
-		local must_reset_on = time()-diff_in_seconds;
-		if (must_reset_on > HonorSpy.db.realm.hs.last_reset) then resetWeek(must_reset_on) end
-	end
-end
-
--- PURGE
-function purgeData()
-	StaticPopup_Show("PURGE_DATA")
-end
-StaticPopupDialogs["PURGE_DATA"] = {
-	text = L["This will purge ALL addon data, you sure?"],
-	button1 = YES,
-	button2 = NO,
-	OnAccept = function()
-		inspectedPlayers = {};
-		HonorSpy.db.realm.hs.currentStandings={};
-		HonorSpyStandings:Refresh();
-		HonorSpy:Print(L["All data was purged"]);
-	end,
-	timeout = 0,
-	whileDead = true,
-	hideOnEscape = true,
-	preferredIndex = 3,
-}
-
--- INSPECTION TRIGGERS
-function HonorSpy:UPDATE_MOUSEOVER_UNIT()
-	if (not paused) then StartInspecting("mouseover") end
-end
-function HonorSpy:PLAYER_TARGET_CHANGED()
-	if (not paused) then
-		if (RealmPlayersAddon) then
-			VF_TemporarySupressTargetChange = true;
-			VF_InspectDone();
-		end
-		StartInspecting("target");
-	end
-end
-
--- GUI
-function HonorSpy:OnClick()
-	checkNeedReset();
-	HonorSpyStandings:Toggle()
-end
-function HonorSpy:OnTooltipUpdate()
-  T:SetHint("by Kakysha, v"..tostring(VERSION))
-end
-
--- PAUSING to not mess with native inspect calls
+-- INSPECT HOOKS pausing to not mess with native inspect calls
 local hooked = false;
 function HonorSpy:InspectUnit(unitID)
 	paused = true;
-	self.hooks["InspectUnit"](unitID)
 	if (not hooked) then
-		self:HookScript(SuperInspectFrame or InspectFrame, "OnHide");
+		self:SecureHookScript(InspectFrame, "OnHide");
 		hooked = true;
 	end
 end
@@ -193,161 +121,36 @@ function HonorSpy:OnHide()
 	paused = false;
 end
 
+-- INSPECTION TRIGGERS
+function HonorSpy:UPDATE_MOUSEOVER_UNIT()
+	if (not paused) then StartInspecting("mouseover") end
+end
+function HonorSpy:PLAYER_TARGET_CHANGED()
+	if (not paused) then StartInspecting("target") end
+end
+
 -- CHAT COMMANDS
-local options = { 
-	type='group',
+local options = {
+	name = 'HonorSpy',
+	type = 'group',
 	args = {
 		show = {
 			type = 'execute',
 			name = L['Show HonorSpy Standings'],
 			desc = L['Show HonorSpy Standings'],
-			func = function() HonorSpyStandings:Toggle() end
+			func = function() HonorSpy:Print(HonorSpy.db.factionrealm.last_reset) end
 		},
 		search = {
-			type = 'text',
+			type = 'input',
 			name = L['Report specific player standings'],
 			desc = L['Report specific player standings'],
 			usage = L['player_name'],
 			get = false,
-			set = function(playerName) HonorSpy:Report(playerName) end
+			set = function(playerName) HonorSpy.db.factionrealm.last_reset=HonorSpy.db.factionrealm.last_reset+1 end
 		},
 	}
 }
-HonorSpy:RegisterChatCommand({"/honorspy", "/hs"}, options)
-
--- REPORT
-function HonorSpy:Report(playerOfInterest)
-	if (not playerOfInterest) then
-		playerOfInterest = playerName
-	end
-	playerOfInterest = string.upper(string.sub(playerOfInterest, 1, 1))..string.lower(string.sub(playerOfInterest, 2))
-
-	local pool_size = 0;
-	local standing = -1;
-	local t = HonorSpyStandings:BuildStandingsTable()
-	local avg_lastchecked = 0;
-	pool_size = table.getn(t);
-	for i = 1, table.getn(t) do
-		if (playerOfInterest == t[i][1]) then
-			standing = i
-		end
-	end
-	if (standing == -1) then
-		self:Print(string.format(L["Player %s not found in table"], playerOfInterest));
-		return
-	end;
-			  -- 1   2     3      4		 5		 6		7		8		9	10		11		12		13	14
-	local brk = {1, 0.858, 0.715, 0.587, 0.477, 0.377, 0.287, 0.207, 0.137, 0.077, 0.037, 0.017, 0.007, 0.002} -- brackets percentage
-	local RP  = {0, 400} -- RP for each bracket
-	local Ranks = {0, 2000} -- RP for each rank
-
-	local my_bracket = 1;
-	local inside_br_progress = 0;
-	for i = 2,14 do
-		brk[i] = math.floor(brk[i]*pool_size+.5);
-		if (standing > brk[i]) then
-			inside_br_progress = (brk[i-1] - standing)/(brk[i-1] - brk[i])
-			break
-		end;
-		my_bracket = i;
-	end
-	if (my_bracket == 14 and standing == 1) then inside_br_progress = 1 end;
-	for i = 3,14 do
-		RP[i] = (i-2) * 1000;
-		Ranks[i] = (i-2) * 5000;
-	end
-	local award = RP[my_bracket] + 1000 * inside_br_progress;
-	local RP = HonorSpy.db.realm.hs.currentStandings[playerOfInterest].RP;
-	local EstRP = math.floor(RP*0.8+award+.5);
-	local Rank = HonorSpy.db.realm.hs.currentStandings[playerOfInterest].rank;
-	local EstRank = 14;
-	local Progress = math.floor(HonorSpy.db.realm.hs.currentStandings[playerOfInterest].rankProgress*100);
-	local EstProgress = math.floor((EstRP - math.floor(EstRP/5000)*5000) / 5000*100);
-	for i = 3,14 do
-		if (EstRP < Ranks[i]) then
-			EstRank = i-1;
-			break;
-		end
-	end
-
-	if (playerOfInterest ~= playerName) then
-		SendChatMessage("- HonorSpy v"..tostring(VERSION)..": "..L["Report for player"].." "..playerOfInterest,"emote")
-	end
-	SendChatMessage("- HonorSpy v"..tostring(VERSION)..": "..L["Pool Size"].." = "..pool_size..", "..L["Standing"].." = "..standing..",  "..L["Bracket"].." = "..my_bracket..",  "..L["current RP"].." = "..RP..",  "..L["Next Week RP"].." = "..EstRP,"emote")
-	SendChatMessage("- HonorSpy v"..tostring(VERSION)..": "..L["Current Rank"].." = "..Rank.." ("..Progress.."%), "..L["Next Week Rank"].." = "..EstRank.." ("..EstProgress.."%)", "emote")
-end
-
--- MINIMAP
-HonorSpy.defaultMinimapPosition = 200
-HonorSpy.cannotDetachTooltip = true
-HonorSpy.tooltipHidderWhenEmpty = false
-HonorSpy.hasIcon = "Interface\\Icons\\Inv_Misc_Bomb_04"
-function BuildMenu()
-	local options = {
-		type = "group",
-		desc = L["HonorSpy options"],
-		args = { }
-	}
-
-	local days = { L["Sunday"], L["Monday"], L["Tuesday"], L["Wednesday"], L["Thursday"], L["Friday"], L["Saturday"] };
-	options.args["reset_day"] = {
-		type = "text",
-		name = L["PvP Week Reset On"],
-		desc = L["Day of week when new PvP week starts (10AM UTC)"],
-		get = function() return days[HonorSpy.db.realm.hs.reset_day+1] end,
-		set = function(v)
-			for k,nv in pairs(days) do
-				if (v == nv) then HonorSpy.db.realm.hs.reset_day = k-1 end;
-			end
-			checkNeedReset();
-		end,
-		validate = days,
-	}
-	options.args["sort"] = {
-		type = "text",
-		name = L["Sort By"],
-		desc = L["Set up sorting column"],
-		get = function() return HonorSpy.db.realm.hs.sort end,
-		set = function(v)
-			HonorSpy.db.realm.hs.sort = v;
-			HonorSpyStandings:Refresh();
-		end,
-		validate = {L["Rank"], L["ThisWeekHonor"]},
-	}
-	options.args["export"] = {
-		type = "execute",
-		name = L["Export to CSV"],
-		desc = L["Show window with current data in CSV format"],
-		func = function() HonorSpy:ExportCSV() end,
-	}
-	options.args["report"] = {
-		type = "execute",
-		name = L["Report My Standing"],
-		desc = L["Reports your current standing as emote"],
-		func = function() HonorSpy:Report() end,
-	}
-	options.args["purge_data"] = {
-		type = "execute",
-		name = L["_ purge all data"],
-		desc = L["Delete all collected data"],
-		func = function() purgeData() end,
-	}
-	options.args["limit"] = {
-		type = "text",
-		name = L["Limit Rows"],
-		desc = L["Limits number of rows shown in table"],
-		get = function() return HonorSpy.db.realm.hs.limit end,
-		set = function(v) HonorSpy.db.realm.hs.limit = v; HonorSpy:Print(L["Limit"].." = "..v) end,
-		usage = L["<EP>"],
-		-- disabled = function() return not (CanEditOfficerNote() and CanEditPublicNote()) end,
-		validate = function(v)
-		  local n = tonumber(v)
-		  return n and n >= 0 and n < 10000
-		end
-	  }
-
-	return options
-end
+LibStub("AceConfig-3.0"):RegisterOptionsTable("HonorSpy", options, {"honorspy", "hs"})
 
 -- SYNCING --
 function table.copy(t)
@@ -359,26 +162,26 @@ end
 function store_player(playerName, player)
 	if (player == nil) then return end
 	
-	if (player.last_checked < HonorSpy.db.realm.hs.last_reset
+	if (player.last_checked < HonorSpy.db.factionrealm.last_reset
 		or player.last_checked > time()
 		or player.thisWeekHonor == 0) then
 		return
 	end
 	
 	local player = table.copy(player);
-	local localPlayer = HonorSpy.db.realm.hs.currentStandings[playerName];
+	local localPlayer = HonorSpy.db.factionrealm.currentStandings[playerName];
 	if (localPlayer == nil or localPlayer.last_checked < player.last_checked) then
-		HonorSpy.db.realm.hs.currentStandings[playerName] = player;
+		HonorSpy.db.factionrealm.currentStandings[playerName] = player;
 	end
 end
 
--- RECEIVE 
---[[function HonorSpy:OnCommReceiveCustom(prefix, sender, distribution, channelName, playerName, player, filtered_players)
-	self:OnCommReceive(prefix, sender, distribution, playerName, player, filtered_players)
-end]]
-function HonorSpy:OnCommReceive(prefix, sender, distribution, playerName, player, filtered_players)
-	if (playerName == false) then
-		for playerName, player in pairs(filtered_players) do
+function HonorSpy:OnCommReceive(prefix, message, distribution, sender)
+	local ok, playerName, player = self:Deserialize(message);
+	if (not ok) then
+		return;
+	end
+	if (playerName == "filtered_players") then
+		for playerName, player in pairs(player) do
 			store_player(playerName, player);
 		end
 		return
@@ -393,20 +196,33 @@ function HonorSpy:PLAYER_DEAD()
 	if (time() - last_send_time < 5*60) then return	end;
 	last_send_time = time();
 
-	for playerName, player in pairs(self.db.realm.hs.currentStandings) do
+	for playerName, player in pairs(self.db.factionrealm.currentStandings) do
 		player.is_outdated = false;
 		filtered_players[playerName] = player;
 		count = count + 1;
 		if (count == 10) then
-			self:SendCommMessage("GROUP", false, false, filtered_players);
-			self:SendCommMessage("GUILD", false, false, filtered_players);
-			-- self:SendCommMessage("CUSTOM", "HS", false, false, filtered_players);
+			self:SendCommMessage(commPrefix, self:Serialize("filtered_players", filtered_players), "GROUP");
+			self:SendCommMessage(commPrefix, self:Serialize("filtered_players", filtered_players), "GUILD");
 			filtered_players, count = {}, 0;
 		end
 	end
 	if (count > 0) then
-		self:SendCommMessage("GROUP", false, false, filtered_players);
-		self:SendCommMessage("GUILD", false, false, filtered_players);
-		-- self:SendCommMessage("CUSTOM", "HS", false, false, filtered_players);
+		self:SendCommMessage(commPrefix, self:Serialize("filtered_players", filtered_players), "GROUP");
+		self:SendCommMessage(commPrefix, self:Serialize("filtered_players", filtered_players), "GUILD");
 	end
+end
+
+-- Minimap icon
+function DrawMinimapIcon()
+	LibStub("LibDBIcon-1.0"):Register("HonorSpy", LibStub("LibDataBroker-1.1"):NewDataObject("HonorSpy",
+	{
+		type = "data source",
+		text = addonName,
+		icon = "Interface\\Icons\\Inv_Misc_Bomb_04",
+		OnClick = function() HonorSpy:Print("test") end,
+		OnTooltipShow = function(tooltip)
+			tooltip:AddLine(string.format("%s", addonName));
+			tooltip:AddLine(string.format("|cff777777by Kakysha|r", GetAddOnMetadata("HonorSpy", "Version")));
+		end
+	}), HonorSpy.db.factionrealm.minimapButton);
 end
